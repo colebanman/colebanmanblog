df18ab80f2381e84c1c6b8399dc9af8747e6c474
colebanman <colebanman@gmail.com>
2024-01-11 19:25:10 -0800

    potential 404 fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Serve index.html for all routes to enable HTML5 history API
+    historyApiFallback: true,
+  },
 })
